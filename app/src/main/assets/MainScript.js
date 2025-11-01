3f7f15e5907f11bdac28c8de616ac1ad6b27c840
TKna3963 <88832283+tkna3963@users.noreply.github.com>
2025-11-01 20:37:52 +0900

    マップ機能を追加して

--- app/src/main/assets/MainScript.js
+++ app/src/main/assets/MainScript.js
@@ -1,1 +1,14 @@
-const localData = Android.getLocalData();
+function KoishiFaceUpdate(Url){
+    document.getElementById("koisi").src=Url;
+}
+
+function GetJson(url){
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        return data;
+    })
+    .catch(error => {
+        console.error('Error fetching JSON:', error);
+    });
+}
